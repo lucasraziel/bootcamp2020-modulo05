5c1bd24c044304df72e4fc6366dcaf7db1780425
Lucas <lucas.raziel@gmail.com>
2020-04-20 20:15:06 -0300

    class 11 - Repositories

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,19 +1,18 @@
 import { Router } from 'express';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
+import { startOfHour, parseISO } from 'date-fns';
 
-import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
-
-const appointments: Appointment[] = [];
+const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
   const parsedDate = startOfHour(parseISO(date));
 
-  const findAppointmentInSameDate = appointments.find((appointment) =>
-    isEqual(appointment.date, parsedDate)
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
+    parsedDate
   );
 
   if (findAppointmentInSameDate) {
@@ -22,9 +21,7 @@ appointmentsRouter.post('/', (request, response) => {
     });
   }
 
-  const appointment = new Appointment(provider, date);
-
-  appointments.push(appointment);
+  const appointment = appointmentsRepository.create(provider, parsedDate);
 
   return response.json(appointment);
 });
