d3b54c963f2a9064553c0af74402909ff7e96c52
Lucas <lucas.raziel@gmail.com>
2020-04-20 19:51:37 -0300

    Class 09 - Validating dates

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,16 +1,35 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface Appointment {
+  date: Date;
+  provider: string;
+  id: string;
+}
+
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointments.find((appointment) =>
+    isEqual(appointment.date, parsedDate)
+  );
+
+  if (findAppointmentInSameDate) {
+    return response.status(400).json({
+      error: 'This appointment is not available',
+    });
+  }
+
   const appointment = {
     provider,
-    date,
+    date: parsedDate,
     id: uuid(),
   };
 
