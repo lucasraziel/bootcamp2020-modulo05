b3f9dc79151ee33d700c7d02b40351bfbaa98c04
Lucas <lucas.raziel@gmail.com>
2020-04-21 21:59:10 -0300

    Class 26 - Upload Files

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express';
+import multer from 'multer';
 import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import uploadConfig from '../config/upload';
 
 const usersRoute = Router();
+const upload = multer(uploadConfig);
 
 usersRoute.post('/', async (request, response) => {
   try {
@@ -22,5 +26,14 @@ usersRoute.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRoute.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    return response.json({ ok: true });
+  }
+);
 
 export default usersRoute;
