2a09a5fb12d9ce605efbbf128a91bd03410e7244
Lucas <lucas.raziel@gmail.com>
2020-04-21 23:05:09 -0300

    Class 28 - Exposing files

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import express from 'express';
 
 import routes from './routes';
 
+import uploadConfig from './config/upload';
+
 import './database';
 
 const app = express();
@@ -11,6 +13,8 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+app.use('/files', express.static(uploadConfig.directory));
 
 app.listen(3333, () => {
   console.log('Server started 🐱‍🏍');
